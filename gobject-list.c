2eb7b7aad640eb5e0822054ca2804a072b1113e1
Danielle Madeley <danielle.madeley@collabora.co.uk>
2011-02-17 21:27:12 +1100

    Add LGPL2.1+ license

--- gobject-list.c
+++ gobject-list.c
@@ -1,3 +1,26 @@
+/*
+ * gobject-list: a LD_PRELOAD library for tracking the lifetime of GObjects
+ *
+ * Copyright (C) 2011  Collabora Ltd.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * USA
+ *
+ * Authors:
+ *     Danielle Madeley  <danielle.madeley@collabora.co.uk>
+ */
 #include <glib-object.h>
 
 #include <dlfcn.h>
